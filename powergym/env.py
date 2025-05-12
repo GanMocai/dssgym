fb7dc72158e534c82a5cb759c950a317c780fbec
GanMocai <su_chen_hui@foxmail.com>
2025-05-12 14:35:18 +0800

    修复本项目未使用的dss_step在启用储能SOC平衡约束后遗留的bug

--- powergym/env.py
+++ powergym/env.py
@@ -904,7 +904,7 @@ class Env(gym.Env):
         terminated = (self.t == self.horizon)  # Episode is done due to termination condition
         truncated = False  # Not truncated due to time limit, etc.
 
-        reward, info = self.reward_func.composite_reward(capdiff, regdiff, soc_errs[:env.sto_num], dis_errs)
+        reward, info = self.reward_func.composite_reward(capdiff, regdiff, soc_errs[self.sto_num], dis_errs)
         # avoid dividing by zero
         # noinspection PyTypeChecker
         info.update({'av_cap_err': sum(capdiff) / (self.cap_num + 1e-10),
